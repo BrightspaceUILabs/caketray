5da81f4764950df4fb0b5c4d803f2202a7918801
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2021-03-02 11:19:02 -0500

    update secondary-card code from foundation-components

--- secondary-card.js
+++ secondary-card.js
@@ -1,34 +1,76 @@
-import { css, html, LitElement } from 'lit-element/lit-element.js';
+
+import '@brightspace-ui-labs/accordion/accordion-collapse.js';
+import { css, html, LitElement } from 'lit-element/lit-element';
+import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
 
 class SecondaryCard extends LitElement {
 
 	static get properties() {
 		return {
-			prop1: { type: String },
+			titleText: { type: String, attribute: 'title-text' },
+			bodyText: { type: String, attribute: 'body-text' },
+			collapsable: { type: Boolean }
 		};
 	}
 
 	static get styles() {
-		return css`
-			:host {
-				display: inline-block;
-			}
+		return [labelStyles, css`
 			:host([hidden]) {
 				display: none;
 			}
-		`;
-	}
-
-	constructor() {
-		super();
-
-		this.prop1 = 'secondary-card';
+			:host {
+				display: flex;
+				flex-direction: column;
+				background: var(--d2l-color-white);
+				border-radius: 8px;
+				margin-bottom: 10px;
+				padding: 20px;
+				padding-top: 0;
+			}
+			.d2l-activity-editor-card {
+				display: grid;
+				grid-template-rows: [start header] auto [content] auto [end];
+			}
+			::slotted([slot=header]) {
+				grid-row: header;
+			}
+			::slotted([slot=content]) {
+				grid-row: content;
+			}
+		`];
 	}
 
 	render() {
+		return this.collapsable ? this._renderCollapsable() : this._renderDefault();
+	}
+
+	_renderDefault() {
 		return html`
-			<h2>Hello ${this.prop1}!</h2>
+			<span slot="header">
+			<h3>
+			${this.titleText}
+			</h3>
+			<hr>
+			</span>
+			<span class="content">
+			${this.bodyText}
+			</span>`;
+	}
+
+	_renderCollapsable() {
+		return html`<d2l-labs-accordion-collapse flex>
+		<span slot="header">
+		<h3>
+		${this.titleText}
+		</h3>
+		<hr>
+		</span>
+		<span class="content">
+		${this.bodyText}
+		</span>
+		</d2l-labs-accordion-collapse>
 		`;
 	}
 }
+
 customElements.define('d2l-labs-secondary-card', SecondaryCard);
