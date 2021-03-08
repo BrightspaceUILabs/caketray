f87a898f435aee7fd87a7e772f45af098f3f72d2
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2021-03-08 15:57:54 -0500

    update test file file to be caketray

--- test/caketray.js
+++ test/caketray.js
@@ -1,19 +1,19 @@
-import '../secondary-card.js';
+import '../caketray.js';
 import { expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
-describe('SecondaryCard', () => {
+describe('Caketray', () => {
 
 	describe('accessibility', () => {
 		it('should pass all aXe tests', async() => {
-			const el = await fixture(html`<d2l-labs-secondary-card></d2l-labs-secondary-card>`);
+			const el = await fixture(html`<d2l-labs-caketray></d2l-labs-caketray>`);
 			await expect(el).to.be.accessible();
 		});
 	});
 
 	describe('constructor', () => {
 		it('should construct', () => {
-			runConstructor('d2l-labs-secondary-card');
+			runConstructor('d2l-labs-caketray');
 		});
 	});
 
